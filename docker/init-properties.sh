a92ed8e94e56c1f0fa28f9a9e09768b5b8c0b9f4
Tim Wuthenow <Tim.Wuthenow@ibm.com>
2022-09-11 20:29:26 -0400

    Adding RHPAM products to init script

--- docker/init-properties.sh
+++ docker/init-properties.sh
@@ -13,15 +13,30 @@ SUPPORT_DIR=./support
 VERSION=8.0
 VERSION_EAP=7.4
 #INSTALL - IBM or Red Hat
+#IF INSTALLING IBM BUSINESS AUTOMATION MANAGER OPEN EDITION LEAVE THESE NEXT LINES UNCOMMENTED, IF RED HAT UNCOMMENT 
+# THOSE BELOW
+#IBM INSTALLATION FILES BEGIN
 INSTALL=IBAMOE
 RHPAM=$INSTALL-$VERSION-BC7.zip
 RHPAM_KIE_SERVER=$INSTALL-$VERSION-KS8.zip
 RHPAM_ADDONS=$INSTALL-$VERSION-AO.zip
 RHPAM_KIE_SERVER=$INSTALL-$VERSION-KS8.zip
+#IBM INSTALLATION FILES END
+
+
+#If the installation is done with the Red Hat Developer downloaded products 
+#(https://developers.redhat.com/products/rhpam/download), uncomment the following:
+#RED HAT INSTALLATION FILES BEGIN
+#VERSION=7.13
+#INSTALL=rhpam
+#RHPAM=rhpam-$VERSION-business-central-eap7-deployable.zip
+#RHPAM_KIE_SERVER=rhpam-$VERSION-kie-server-ee8.zip
 #RHPAM_ADDONS=rhpam-$VERSION-add-ons.zip
+#END RED HAT INSTALLATION FILES
+
 PAM_CASE_MGMT=rhpam-7.7.0-case-mgmt-showcase-eap7-deployable.zip
 EAP=jboss-eap-$VERSION_EAP.zip
-#EAP_PATCH=jboss-eap-6.4.7-patch.zip
+
 #PROJECT_GIT_REPO=https://github.com/jbossdemocentral/rhpam7-mortgage-demo-repo
 #PROJECT_GIT_REPO_NAME=examples-rhpam7-mortgage-demo-repo.git
 #NIOGIT_PROJECT_GIT_REPO="MySpace/$PROJECT_GIT_REPO_NAME"
